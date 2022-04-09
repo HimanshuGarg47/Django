2a94881c6fc449bb91b7514c80271313f45ec758
Himanshuga08 <himan.gargcoder@gmail.com>
2022-04-09 07:01:50 +0530

    Add Address, Author models and Admin Functionality

--- BOOK_STORE/book_outlet/admin.py
+++ BOOK_STORE/book_outlet/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 
 # Register your models here.
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    prepopulated_fields = {"slug":("title",)}
+    list_filter = ("author","rating",)
+    list_display = ("title","author")
+admin.site.register(Author)
+admin.site.register(Book, BookAdmin)
--- BOOK_STORE/book_outlet/models.py
+++ BOOK_STORE/book_outlet/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from wsgiref.validate import validator
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
@@ -6,12 +7,32 @@ from django.utils.text import slugify
 
 
 # Create your models here.
+class Address(models.Model):
+    street = models.CharField(max_length=60)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+    
+    
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address,on_delete=models.CASCADE,null=True)
+    
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    
+    
+    def __str__(self):
+        return self.full_name()
+    
+    
 class Book(models.Model):
     title = models.CharField( max_length=50)
     rating  = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author , on_delete= models.CASCADE , null = True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", blank=True, editable=False, null=False, db_index=True)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
     
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
@@ -21,9 +42,9 @@ class Book(models.Model):
         return f"{self.title} ({self.rating} ) [{self.is_bestselling}]"
     
     
-    def save(self, *args , **kwargs):
-        self.slug = slugify(self.title)   #  slugify(' Joel is a slug ')  'joel-is-a-slug'
-        super().save(*args, **kwargs)
+    # def save(self, *args , **kwargs):
+    #     self.slug = slugify(self.title)   #  slugify(' Joel is a slug ')  'joel-is-a-slug'
+    #     super().save(*args, **kwargs)
         
         
         
